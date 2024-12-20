d847145639ad41dcb338b62c6c544453d64158fd
DasunNirmal <dasun200301@gmail.com>
2024-12-20 10:05:52 +0530

    updated

--- src/intro.tsx
+++ src/intro.tsx
@@ -225,3 +225,37 @@ import {Customer} from "./component/Customer.tsx";*/
 // }
 //
 // export default App
+
+
+// import './App.css'
+// import {createBrowserRouter, RouterProvider} from "react-router";
+// import Dashboard from "./pages/Dashboard.tsx";
+// import AddCustomer from "./pages/AddCustomer.tsx";
+// import UpdateCustomer from "./pages/UpdateCustomer.tsx";
+// import DeleteCustomer from "./pages/DeleteCustomer.tsx";
+// import {RootLayout} from "./component/RootLayout.tsx";
+// import {CustomerProvider} from "./component/CustomerProvider.tsx";
+//
+// function App() {
+//     /*Route is a destination*/
+//
+//     const routers =  createBrowserRouter([
+//         {path : '',
+//             element : <RootLayout/>,
+//             children:[
+//                 {path : '',element : <Dashboard/>},
+//                 {path : '/add',element:<AddCustomer/>},
+//                 {path : '/update',element:<UpdateCustomer/>},
+//                 {path : '/delete',element:<DeleteCustomer/>}
+//             ]},
+//     ])
+//
+//     return (
+//         <>
+//             <CustomerProvider>
+//                 <RouterProvider router={routers}/>
+//             </CustomerProvider>
+//         </>
+//     )
+// }
+// export default App
