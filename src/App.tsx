eeeabfb2f234c3f17b2b55176acd0d2bb7bfc129
DasunNirmal <dasun200301@gmail.com>
2024-12-20 10:32:33 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,6 @@
 import './App.css'
 import {useReducer} from "react";
-
-const initialState = 0;
-
-function CountReducer (state = initialState, action: {type: string, payload: number}) {
-    switch (action.type) {
-        case 'ADD':
-            return state + action.payload;
-        case 'REMOVE':
-            return state - action.payload;
-        default:
-            return state;
-    }
-}
+import {CountReducer, initialState} from "./reducers/CountReducer.ts";
 
 function App() {
     const [count,dispatch] = useReducer(CountReducer,initialState);
